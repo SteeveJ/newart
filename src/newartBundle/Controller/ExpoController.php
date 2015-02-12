afb6c38290578e17ad7c63d347cb1ef9a2f43238
Steeve Jerent <jerent.steeve@gmail.com>
2015-02-12 10:24:10 +0100

    integration des template existant

---
+++ src/newartBundle/Controller/ExpoController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace newartBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class ExpoController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('newartBundle:Expo:index.html.twig');
+    }
+
+    public function profilAction()
+    {
+        return $this->render('newartBundle:Expo:profile.html.twig');
+    }
+
+    public function booksAction()
+    {
+        return $this->render('newartBundle:Expo:books.html.twig');
+    }
+
+    public function expoAction()
+    {
+        return $this->render('newartBundle:Expo:expo.html.twig');
+    }
+
+    public function naeAction()
+    {
+        return $this->render('newartBundle:Expo:nae.html.twig');
+    }
+
+    public function contactAction()
+    {
+        return $this->render('newartBundle:Expo:contact.html.twig');
+    }
+
+    public function articlesAction()
+    {
+        return $this->render('newartBundle:Expo:articles.html.twig');
+    }
+
+    public function articleAction()
+    {
+        return $this->render('newartBundle:Expo:article.html.twig');
+    }
+
+
+    public function cvgAction()
+    {
+        return $this->render('newartBundle:Expo:cvg.html.twig');
+    }
+}
